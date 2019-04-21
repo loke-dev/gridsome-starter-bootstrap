335a3ebb58f3eadd9c494103af61ff6d059d289b
Loke Carlsson <carlssonloke@gmail.com>
2019-04-21 11:23:10 +0200

    Add bootstrap

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,11 +2,12 @@
   <div class="layout">
     <header class="header">
       <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
+        <g-link class="text-xl" to="/">{{ $static.metaData.siteName }}</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
+         
+        <g-link class="nav__link" to="/"><b-button variant="link">Home</b-button></g-link>
+        <g-link class="nav__link" to="/about"><b-button variant="link">About</b-button></g-link>
       </nav>
     </header>
     <slot/>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import DefaultLayout from '~/layouts/Default.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+import './assets/styles/main.scss'
+
 export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
+  Vue.use(BootstrapVue)
   Vue.component('Layout', DefaultLayout)
 }
