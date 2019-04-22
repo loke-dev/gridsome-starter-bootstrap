d721094fbf97e48064261a00106b71bb252f7ac1
Loke Carlsson <carlssonloke@gmail.com>
2019-04-22 11:34:23 +0200

    Add success page

---
+++ src/pages/success.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Success!</h1>
+    <p>Your message have been successfully sent!</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Success'
+  }
+}
+</script>
