a01e9f82799c570ee088ec41df490f14d19a5be4
Loke Carlsson <carlssonloke@gmail.com>
2019-04-22 16:42:32 +0200

    Add GA and sitemap generator

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   siteName: 'Gridsome starter bootstrap',
-  plugins: [],
+  siteDescription: 'A starter project for Gridsome with Bootstrap and some other useful tools.',
+  siteUrl: 'https://gridsome-starter-bootstrap.loke.dev',
+  plugins: [
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-72659574-10'
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000
+      }
+    }
+  ],
   css: {
     loaderOptions: {
       scss: {
