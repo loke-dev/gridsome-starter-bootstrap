f437b97f9a3982f174be528ce8ee85c8ae29ffc2
Loke Carlsson <carlssonloke@gmail.com>
2019-04-22 10:25:13 +0200

    Update name

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,11 +1,5 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Gridsome starter bootstrap',
   plugins: [],
   css: {
     loaderOptions: {
