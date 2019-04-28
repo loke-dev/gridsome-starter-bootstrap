29b2caa2e43a30d77133d74005ff5691a3a608ce
Loke Carlsson <carlssonloke@gmail.com>
2019-04-28 11:17:48 +0200

    Update colors

---
+++ src/pages/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <div class="d-flex">
+    <g-image class="" src="../assets/images/undraw_page_not_found_su7k.svg" />
+    </div>
+  </Layout>
+</template>
+
+<style scoped lang="scss">
+img {
+  width: 80%;
+  margin: auto;
+
+  @media (max-width: 500px) {
+    width: 90%;
+  }
+  @media (max-width: 400px) {
+    width: 100%;
+  }
+}
+</style>
