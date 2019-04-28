57b8bc79083ed7616b8f45594b958366b12469fe
Loke Carlsson <carlssonloke@gmail.com>
2019-04-28 10:31:08 +0200

    Add mobile check mixin

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import DefaultLayout from '~/layouts/Default.vue'
 import BootstrapVue from 'bootstrap-vue'
 import './assets/styles/main.scss'
 
+import checkIfMobile from './mixins/checkIfMobile'
+
 export default function (Vue, { router, head, isClient }) {
   Vue.use(BootstrapVue)
   Vue.component('Layout', DefaultLayout)
+  Vue.mixin(checkIfMobile)
 }
