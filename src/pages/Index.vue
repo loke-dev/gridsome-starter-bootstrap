91008f561b5bc8a55ff5ce8a9da94d1aa95e6df8
Loke Carlsson <carlssonloke@gmail.com>
2019-04-28 16:10:01 +0200

    Revert

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <g-image immediate class="greet-image" src="../assets/images/hello.svg" />
+    <img class="greet-image" src="../assets/images/hello.svg" />
 
     <h2 class="mb-4">Gridsome starter bootstrap</h2>
     <p>
